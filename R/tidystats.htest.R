054e0e4a569439e6a596f30539b79573bc37381c
Arnoud Plantinga <arnoudplantinga@gmail.com>
2017-07-31 15:21:44 +0200

    Changed dependencies

--- R/tidystats.htest.R
+++ R/tidystats.htest.R
@@ -2,12 +2,14 @@
 #'
 #' Creates a tidystats data.frame for an htest object. 
 
-#' @Dependencies
-#' dplyr, broom
-
 #' @examples
 #' ttest <- t.test(1:10, y = c(7:20))
 #' tidystats.htest("ttest_x_y", ttest, "A t-test for x and y")
+#' 
+#'@import dplyr
+#'@import broom
+#'@import tibble
+#'@importFrom magrittr %>%
 
 tidystats.htest <- function(identifier, model, description = NULL) {
     # Tidy the result to a data.frame
