5c3a4c5b629b60e2855cfd2c73076fd9b2127c7f
Willem Sleegers <w.sleegers@me.com>
2017-07-31 15:07:08 +0200

    updated correlation function

--- R/correlation.R
+++ R/correlation.R
@@ -2,9 +2,6 @@
 #'
 #' Create tidy correlation output.
 
-#' @Dependencies
-#' tibble, dplyr, broom
-
 #' @examples
 #' x <- c(44.4, 45.9, 41.9, 53.3, 44.7, 44.1, 50.7, 45.2, 60.1)
 #' y <- c( 2.6,  3.1,  2.5,  5.0,  3.6,  4.0,  5.2,  2.8,  3.8)
@@ -12,6 +9,12 @@
 #' model <- cor.test(x, y)
 #' correlation("M1", "hypothesis test", model)
 
+#'@import dplyr
+#'@import broom
+#'@import tibble
+#'@importFrom magrittr %>%
+
+#'@export
 correlation <- function(identifier, type, model, description = NULL) {
     
     # Use the broom's tidy() function to retrieve relevant statistics, add the identifier and type information, and store the results in a data frame
