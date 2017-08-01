d2b7538b9eb00d096f254d61ac07aaf39a893422
Arnoud Plantinga <arnoudplantinga@gmail.com>
2017-08-01 12:07:30 +0200

    Use case_when

--- R/report.R
+++ R/report.R
@@ -8,11 +8,13 @@
 #'@export
 report <- function(results, identifier, term = NULL, statistic = NULL) {
     # Find out which test was used
-    testmethod <- filter(results, identifier == identifier) %>%
+    method <- filter(results, identifier == identifier) %>%
         pull(method)
 
     # Run the appropriate report function
-    if (testmethod == "Welch Two Sample t-test") {
-        report_t_test(results, identifier, statistic)
-    }
+    output <- case_when(
+        grepl("t-test", method) ~ report_t_test(results, identifier, statistic)
+    )
+    
+    return(output)
 }
