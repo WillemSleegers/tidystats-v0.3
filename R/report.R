07d644f0372c54d26093ae798ec3719b554394c5
Arnoud Plantinga <arnoudplantinga@gmail.com>
2017-08-01 11:35:39 +0200

    Added report_t_test function

--- R/report.R
+++ R/report.R
@@ -10,7 +10,7 @@ report <- function(results, identifier, term = NULL, statistic = NULL) {
     # Find out which test was used
     testmethod <- filter(results, identifier == identifier) %>%
         pull(method)
-    
+
     # Run the appropriate report function
     if (testmethod == "Welch Two Sample t-test") {
         report_t_test(results, identifier, statistic)
