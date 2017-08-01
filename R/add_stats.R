5c61c6cffa369dfe71b733bcf199c8889b0464d4
Arnoud Plantinga <arnoudplantinga@gmail.com>
2017-08-01 16:04:09 +0200

    Create an identifier if not specified

--- R/add_stats.R
+++ R/add_stats.R
@@ -10,7 +10,13 @@
 #'@import dplyr
 
 #'@export
-add_stats <- function(results, model, identifier, type = "other", description = NULL) {
+add_stats <- function(results, model, identifier = NULL, type = "other", description = NULL) {
+
+  # Create an identifier if it is not specified
+  if (is.null(identifier)) {
+    identifier <- formatC(nrow(results), width = "2", format = "d", flag = "0") %>%
+      paste("Model", .)
+  }
 
   # Create the new row
   new_row <- tidystats(model, identifier, type, description) %>%
