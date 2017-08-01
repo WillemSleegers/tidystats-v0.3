724adbd820e39caf10e62f5f504f0a8bed54f631
Willem Sleegers <w.sleegers@me.com>
2017-08-01 11:39:48 +0200

    Added helper function to report p values in APA style

---
+++ R/report_p_value.R
@@ -0,0 +1,18 @@
+#' Report p-value function
+#'
+#' A helper function to report p values in APA style. 
+
+#' @examples
+#' report_p_value(.45)
+#' report_p_value(.002)
+#' report_p_value(.00000122)
+
+#'@export
+report_p_value <- function(p_value) {
+    
+    if (p_value >= .001) {
+        paste("*p* =", gsub(format(p_value, digits = 2), "0\\.", ".")) 
+    } else {
+        paste("*p* < .001")
+    }
+}
