cfdb3ab28144e6fb43ce8d14db0037fba8c7533a
Arnoud Plantinga <arnoudplantinga@gmail.com>
2017-08-01 10:11:36 +0200

    Changed addstats to add_stats

--- R/add_stats.R
+++ R/add_stats.R
@@ -1,26 +1,26 @@
-#' Addstats generic function
+#' add_stats Generic Function
 #'
-#' \code{addstats} is a generic function to add a line of tidy stats to a tidy stats data.frame.
+#' \code{add_stats} is a generic function to add a line of tidy stats to a tidy stats data.frame.
 
 #' @examples
 #' results <- new_stats_data_frame()
 #' ttest <- t.test(1:10, y = c(7:20))
-#' results <- addstats(results, ttest, "ttest_x_y", "Hypothesis", "A t-test of x and y")
+#' results <- add_stats(results, ttest, "ttest_x_y", "Hypothesis", "A t-test of x and y")
 
 #'@import dplyr
 
 #'@export
-addstats <- function(results, model, identifier, type = "", description = NULL) {
-    
+add_stats <- function(results, model, identifier, type = "", description = NULL) {
+
     # Create the new row
-    newrow <- tidystats(model, identifier, type, description) %>% 
+    newrow <- tidystats(model, identifier, type, description) %>%
         mutate_if(is.factor, as.character)
-    
+
     # Check if the identifier already exists
-    if(identifier %in% results$identifier) {
+    if (identifier %in% results$identifier) {
         stop("Identifier already exists.")
     }
-    
+
     # Add the new row to the data.frame
     if (is.na(results[1, 1])) {
         # Overwrite the existing result
@@ -28,7 +28,7 @@ addstats <- function(results, model, identifier, type = "", description = NULL) 
     } else {
         newresults <- bind_rows(results, newrow)
     }
-    
+
     # Return the new results data.frame
     return(newresults)
 }
