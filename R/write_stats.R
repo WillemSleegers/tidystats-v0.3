a5f84801306b94d586b643c585b17a66f848a15a
Willem Sleegers <w.sleegers@me.com>
2017-08-02 22:16:47 +0200

    Updated write_stats() to include identifiers

--- R/write_stats.R
+++ R/write_stats.R
@@ -3,14 +3,19 @@
 #' \code{write_stats} creates a \code{.csv} file with all stats
 
 #' @examples
-#' results <- new_stats_data_frame()
-#' ttest <- t.test(1:10, y = c(7:20))
-#' results <- add_stats(results, ttest, "ttest_x_y", "Hypothesis", "A t-test of x and y")
+#' results <- new_stats_list()
+#' model <- t.test(1:10, y = c(7:20))
+#' results <- add_stats(results, model, "model1", "hypothesis")
+#' write_stats(results)
 
-#'@import dplyr
+#' @import readr
+#' @import dplyr
 
 #'@export
 write_stats <- function(results, file) {
   df <- bind_rows(results)
-  write.csv(df, file = file, row.names = FALSE, na = "")
+  df$identifier <- names(results)
+  df <- select(df, identifier, everything())
+
+  write_csv(df, file = file, na = "")
 }
