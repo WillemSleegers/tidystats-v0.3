ca022839c5b67b3c3d63dfc40833d32739ddc21d
Arnoud Plantinga <arnoudplantinga@gmail.com>
2017-08-02 11:33:47 +0200

    Added a simple write_stats function

---
+++ R/write_stats.R
@@ -0,0 +1,16 @@
+#' Write a .csv file with all stats
+#'
+#' \code{write_stats} creates a \code{.csv} file with all stats
+
+#' @examples
+#' results <- new_stats_data_frame()
+#' ttest <- t.test(1:10, y = c(7:20))
+#' results <- add_stats(results, ttest, "ttest_x_y", "Hypothesis", "A t-test of x and y")
+
+#'@import dplyr
+
+#'@export
+write_stats <- function(results, file) {
+  df <- bind_rows(results)
+  write.csv(df, file = file, row.names = FALSE)
+}
