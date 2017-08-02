5856092462ab1173bc02afb75e1b79802ff31595
Willem Sleegers <w.sleegers@me.com>
2017-08-02 22:57:47 +0200

    Updated report.htest

--- R/report.htest.R
+++ R/report.htest.R
@@ -1,37 +1,58 @@
-#' Report method for a t-test.
+#' Report method for a t-test or correlation.
 #'
-#' Function to report a t-test in APA style.
-
-# @examples
-# ttest <- t.test(1:10, y = c(7:20))
-# tidystats.htest("ttest_x_y", "Hypothesis", ttest, "A t-test for x and y")
+#' Function to report a t-test or correlation in APA style.
 #'
-#'@import dplyr
-#'@import broom
-#'@import tibble
-#'@importFrom magrittr %>%
+#' @examples
+#' report.htest(results, "model1")
+#'
+#' @import dplyr
+#' @importFrom magrittr %>%
+#'
+#' @export
+report.htest <- function(results, identifier, statistic = NULL) {
 
-#'@export
-report.htest <- function(model, statistic = NULL) {
-  # Create a results data.frame
-  results <- new_stats_data_frame() %>%
-    # Add the t-test info
-    add_stats(model, "tmp")
+  # Extract the results of the specific model through its identifier
+  res <- results[[identifier]]
 
-  # Get the parameters
-  res <- results %>%
-    select(parameter, estimate, p.value, cohens_d) %>%
-    mutate_at(vars(parameter, cohens_d), ~ formatC(., digits = 2, format = "f")) %>%
-    mutate(p.value = report_p_value(p.value))
-
-  # Report the test
-  if (is.null(statistic)) {
-    with(res,
-      paste0("*t*(", res$parameter, ") = ", estimate, ", ", p.value, ", *d* = ",
-           cohens_d)
-    )
+  # Check if only a single statistic is asked, otherwise produce full line of APA results
+  if (!is.null(statistic)) {
+    output <- res[statistic]
   } else {
-  # Report a specific statistic
-    res[statistic]
+    if (grepl("t-test", res$method)) {
+      res <- res %>%
+        mutate_at(vars(cohens_d), ~ formatC(., digits = 2, format = "f")) %>%
+        mutate(df = if_else(grepl("Welch", method),
+                            formatC(df, digits = 2, format = "f"),
+                            formatC(df, digits = 0, format = "f"))) %>%
+        mutate(p_value = report_p_value(p_value)) %>%
+        select(df, estimate, p_value, cohens_d)
+
+      output <- with(res,
+           paste0("*t*(", res$df, ") = ", estimate, ", ", p_value, ", *d* = ",
+                  cohens_d)
+      )
+    } else {
+      if (grepl("Pearson"), res$method) {
+        res <- res %>%
+          mutate_at(vars(estimate), ~ formatC(., digits = 2, format = "f")) %>%
+          mutate(p_value = report_p_value(p_value)) %>%
+          select(df, estimate, p_value)
+
+        output <- with(res, paste0("*r*(", res$df, ") = ", estimate, ", ", p_value))
+      } else {
+        if (grepl("Kendall"), res$method) {
+          res <- res %>%
+            mutate_at(vars(estimate), ~ formatC(., digits = 2, format = "f")) %>%
+            mutate(p_value = report_p_value(p_value)) %>%
+            select(df, estimate, p_value)
+
+          output <- with(res, paste0("*r<sub>$latex\tau</sub>* = ", estimate, ", ", p_value))
+        }
+      }
+    }
+
+
   }
+
+  return(output)
 }
