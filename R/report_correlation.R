a483a70a29e65b695eb2149ef104a7847f044954
Willem Sleegers <w.sleegers@me.com>
2017-08-02 23:30:40 +0200

    Updated report_correlation

--- R/report_correlation.R
+++ R/report_correlation.R
@@ -19,23 +19,41 @@
 
 #'@export
 report_correlation <- function(results, ID, statistic = NULL) {
-  # Get the parameters
-  res <- filter(results, identifier == ID)
 
-  if (res$method == "Pearson's product-moment correlation") {
-    res %>%
-      select(estimate, parameter, p_value) %>%
-      mutate_at(vars(estimate, parameter), ~ formatC(., digits = 2, format = "f")) %>%
-      mutate(p_value = report_p_value(p_value)) -> res
-  }
+  # Extract the results of the specific model through its identifier
+  res <- results[[identifier]]
 
-  # Report the test
-  if (is.null(statistic)) {
-    with(res,
-         paste0("*r*(", res$parameter, ") = ", estimate, ", ", p_value)
-    )
+  # Check if only a single statistic is asked, otherwise produce a full line of APA results
+  if (!is.null(statistic)) {
+    output <- res[statistic]
   } else {
-    # Report a specific statistic
-    res[statistic]
+
+    if (grepl("Pearson", res$method)) {
+      res <- res %>%
+        mutate(estimate = formatC(estimate, digits = 2, format = "f")) %>%
+        mutate(df = formatC(df, digits = 0, format = "f")) %>%
+        mutate(p_value = report_p_value(p_value))
+
+      output <- with(res,
+                     paste0("*r*(", res$df, ") = ", estimate, ", ", p_value)
+      )
+    } else {
+      res <- res %>%
+        mutate(estimate = formatC(estimate, digits = 2, format = "f")) %>%
+        mutate(p_value = report_p_value(p_value))
+
+      if (grepl("Kendall", res$method)) {
+        output <- with(res,
+                       paste0("*r<sub>$\tau$</sub>* = ", estimate, ", ", p_value)
+        )
+      } else {
+        output <- with(res,
+                       paste0("*r<sub>test</sub>*(", res$df, ") = ", estimate, ", ", p_value)
+        )
+      }
+    }
+
   }
+
+  return(output)
 }
