25ef29fa491dd27444305f80f4ac4a4e2df06bd2
WillemSleegers <w.sleegers@me.com>
2017-08-03 09:14:44 +0200

    Added a list_to_df function

---
+++ R/list_to_df.r
@@ -0,0 +1,17 @@
+#' Convert a tidystats list to a data frame
+#'
+#' \code{list_to_df} converts a tidystats list to a data frame
+
+#'@examples
+#' list_to_df(results)
+
+#'@import dplyr
+
+#'@export
+list_to_df <- function(results) {
+  df <- bind_rows(results)
+  df$identifier <- names(results)
+  df <- select(df, identifier, everything())
+
+  return(df)
+}
