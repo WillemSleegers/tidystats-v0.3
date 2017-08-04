5a47102b559b97bb6346793a20fc5ebffa396695
Willem Sleegers <w.sleegers@me.com>
2017-08-04 12:28:56 +0200

    Implemented tidy stats method for lm models

--- R/tidy_stats.lm.r
+++ R/tidy_stats.lm.r
@@ -1,6 +1,6 @@
 #' tidy_stats method for an lm object
 #'
-#' Creates a tidy_stats data frame for an lm object.
+#' Creates a tidystats data frame for an lm object.
 
 #' @examples
 #' lm_model <- lm(extra ~ group, data = sleep)
@@ -10,28 +10,44 @@
 #'@importFrom magrittr %>%
 
 #'@export
-tidy_stats.lm <- function(model, identifier, type = "other", description = NULL) {
+tidy_stats.lm <- function(model) {
+  # Create tidy stats data frame
+  output <- data_frame(
+    method = rep("Linear regression", length(model$coefficients))
+  )
 
-  # Tidy the result to a data.frame
-  tidy(model) %>%
-    rename(
-      p_value = p.value,
-      std_error = std.error) %>%
-    select(one_of("term", "estimate", "std_error", "statistic", "p_value")) %>%
-    mutate(
-      identifier = identifier,
-      type = type,
-      method = "Linear regression"
-    ) %>%
-    select(identifier, type, method, term, everything()) -> output
+  # Add term(s)
+  output$term <- names(model$coefficients)
 
-  # Add description if provided
-  if (!is.null(description)) {
-    output %>%
-      mutate(
-        description = description
-      ) -> output
-  }
+  # Add estimates, standard errors, t values, and p values
+  output <- bind_cols(output, as_data_frame(summary(model)$coefficients))
+
+  # Add df
+  output$df_res <- model$df.residual
+
+  # Rename variables
+  output <- rename(output,
+                   estimate = Estimate,
+                   std_error = `Std. Error`,
+                   statistic = `t value`,
+                   p_value = `Pr(>|t|)`)
+
+  # Create a separate data frame to store the model fit statistics in
+  output_fit <- data_frame(
+    method = "Linear regression",
+    term = "(Model)",
+    statistic = summary(model)$fstatistic[1],
+    df = summary(model)$fstatistic[2],
+    df_res = summary(model)$fstatistic[3],
+    r_squared = summary(model)$r.squared,
+    adjusted_r_squared = summary(model)$adj.r.squared
+  )
+
+  output_fit$p_value <- pf(output_fit$statistic, output_fit$df, output_fit$df_res,
+                           lower.tail = FALSE)
+
+  # Add the model fit results to the coefficients results
+  output <- bind_rows(output, output_fit)
 
   return(output)
 }
