2f870ab4f7a3b7a5bf00dcaaa6c9171d8b6c2736
WillemSleegers <w.sleegers@me.com>
2017-08-04 20:16:30 +0200

    Renamed to stats_list_to_df

--- R/list_to_df.r
+++ R/list_to_df.r
@@ -1,20 +1,21 @@
 #' Convert a tidystats list to a data frame
 #'
-#' \code{list_to_df} converts a tidystats list to a data frame
+#' \code{stats_list_to_df} converts a tidystats list to a data frame
 #'
 #'@examples
-#' list_to_df(results)
+#' stats_list_to_df(results)
 #'
 #'@import dplyr
+#'@importFrom magrittr %>%
 #'@import purrr
 #'
 #'@export
-list_to_df <- function(results) {
+stats_list_to_df <- function(results) {
 
   # Loop over each element in the list and add the identifier information, then reorder
-  results %>%
+  df <- results %>%
     map2_df(names(results), mutate, identifier = `.y[[i]]`) %>%
-    select(identifier, method, everything(), -`.y[[i]]`) -> temp
+    select(identifier, method, everything(), -`.y[[i]]`)
 
   return(df)
 }
