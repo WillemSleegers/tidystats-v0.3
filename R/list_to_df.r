754e602a517bb1c3257a219bbf258057b3ed207c
Willem Sleegers <w.sleegers@me.com>
2017-08-04 10:21:43 +0200

    Updated documentation

--- R/list_to_df.r
+++ R/list_to_df.r
@@ -1,12 +1,12 @@
 #' Convert a tidystats list to a data frame
 #'
 #' \code{list_to_df} converts a tidystats list to a data frame
-
+#'
 #'@examples
 #' list_to_df(results)
-
+#'
 #'@import dplyr
-
+#'
 #'@export
 list_to_df <- function(results) {
   df <- bind_rows(results)
