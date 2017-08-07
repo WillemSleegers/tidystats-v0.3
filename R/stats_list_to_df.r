08447861194c13ec24123490393757a3383b5482
Willem Sleegers <w.sleegers@me.com>
2017-08-07 13:03:38 +0200

    Bug fix for when estimate or std_error is requested but doesn't exist

--- R/stats_list_to_df.r
+++ R/stats_list_to_df.r
@@ -15,8 +15,8 @@ stats_list_to_df <- function(results) {
   # Loop over each element in the list and add the identifier information, then reorder columns
   df <- results %>%
     map2_df(names(results), mutate, identifier = `.y[[i]]`) %>%
-    select(identifier, method, one_of("term"), estimate, std_error, statistic, one_of("df_model"),
-           df_error, everything(), -`.y[[i]]`)
+    select(identifier, method, one_of("term", "estimate", "std_error"),
+           statistic, one_of("df_model"), df_error, everything(), -`.y[[i]]`)
 
   return(df)
 }
