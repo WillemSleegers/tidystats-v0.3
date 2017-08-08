df6683840d4ef25daf7fed51c92c52aeb206d968
Willem Sleegers <w.sleegers@me.com>
2017-08-08 12:47:56 +0200

    Changed name of file argument to path and updated documentation

--- R/write_stats.R
+++ R/write_stats.R
@@ -1,19 +1,19 @@
-#' Write a .csv file with all stats
+#' Write a csv file with all stats
 #'
-#' \code{write_stats} creates a \code{.csv} file with all stats
-
+#' \code{write_stats} creates a \code{csv} file with all stats
+#'
 #' @examples
 #' results <- new_stats_list()
 #' model <- t.test(1:10, y = c(7:20))
 #' results <- add_stats(results, model, "model1", "hypothesis")
-#' write_stats(results)
-
+#' write_stats(results, "results.csv)
+#'
 #' @import readr
 #' @import dplyr
-
-#'@export
-write_stats <- function(results, file) {
+#'
+#' @export
+write_stats <- function(results, path) {
   df <- stats_list_to_df(results)
 
-  write_csv(df, path = file, na = "")
+  write_csv(df, path = path, na = "")
 }
