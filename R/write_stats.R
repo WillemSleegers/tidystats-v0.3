ca5f2dab476b60911b86a7ed24e28342e7986f41
WillemSleegers <w.sleegers@me.com>
2017-08-09 12:44:16 +0200

    Updated documentation

--- R/write_stats.R
+++ R/write_stats.R
@@ -1,15 +1,14 @@
 #' Write a csv file with all stats
 #'
-#' \code{write_stats} creates a \code{csv} file with all stats
+#' \code{write_stats} creates a .csv file with all stats
 #'
 #' @examples
-#' results <- new_stats_list()
-#' model <- t.test(1:10, y = c(7:20))
-#' results <- add_stats(results, model, "model1", "hypothesis")
-#' write_stats(results, "results.csv)
+#' results <- list()
+#' model <- t.test(1:10, y = 7:20)
+#' results <- add_stats(results, model)
+#' write_stats(results, "results.csv")
 #'
 #' @import readr
-#' @import dplyr
 #'
 #' @export
 write_stats <- function(results, path) {
