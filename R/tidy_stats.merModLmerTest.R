22035d896b9cdaeb6002076a3bff119b9155b927
WillemSleegers <w.sleegers@me.com>
2017-08-12 00:43:11 +0200

    Added lmerTest support

---
+++ R/tidy_stats.merModLmerTest.R
@@ -0,0 +1,67 @@
+#' tidy_stats method for merModLmerTest objects
+#'
+#' Creates a tidystats data frame for an merModLmerTest object.
+#'
+#' @param model An merModLmerTest object
+#'
+#' @examples
+#' library(lme4)
+#' library(lmerTest)
+#'
+#' lme4_model <- lmer(extra ~ group + (1|ID), data = sleep)
+#' tidy_stats.lmerMod(lme4_model)
+#'
+#' @import dplyr
+#' @import tidyr
+#' @importFrom magrittr %>%
+#'
+#' @export
+tidy_stats.merModLmerTest <- function(model) {
+
+  # Get summary statistics
+  summary <- summary(model)
+
+  # Extract statistics of random effects
+  random <- as_data_frame(summary$varcor) %>%
+    mutate(
+      term = paste("(R)", grp, var1, var2, sep = "-"),
+      term = gsub("-NA", "", term),
+      order = 1:n()
+    ) %>%
+    rename(
+      variance = vcov,
+      SD = sdcor
+    ) %>%
+    gather("statistic", "value", variance, SD) %>%
+    arrange(order) %>%
+    select(-order, -grp, -var1, -var2)
+
+  # Extract statistics of fixed effects
+  fixed <- as_data_frame(summary$coefficients) %>%
+    mutate(
+      term = paste("(F)", rownames(summary$coefficients), sep = "-"),
+      order = 1:n()
+    ) %>%
+    rename(
+      estimate = `Estimate`,
+      SE = `Std. Error`,
+      t = `t value`,
+      p = `Pr(>|t|)`
+    ) %>%
+    gather("statistic", "value", -term, -order) %>%
+    arrange(order) %>%
+    select(-order)
+
+  # Combine both
+  output <- bind_rows(random, fixed)
+
+  # Not included:
+  # - Scaled residuals
+  # - Number of obs
+  # - Correlation of Fixed Effects
+
+  # Add method
+  output$method <- "Linear mixed model"
+
+  return(output)
+}
