7bfd102a0e3487ee4529e3e51555777a620d2a65
Willem Sleegers <w.sleegers@me.com>
2017-10-02 03:09:44 +0200

    Added the 'cox' data set to the package to use in examples.

---
+++ R/cox.R
@@ -0,0 +1,8 @@
+#' Data of Wissink, Hoogendoorn, Brohmer, Verschoor, Krijnen, & Zeelenberg
+#'
+#' @description Replication of “Terror management and adults’ attachment to their parents: The safe haven remains” by CR Cox, J Arndt, T Pyszczynski, J Greenberg, A Abdollahi, S Solomon (2008, Journal of Personality and Social Psychology).
+#'
+#' @format A data frame with 200 rows and 67 variables:
+#'
+#' @references \url{https://osf.io/5tbxf/}
+"cox"
