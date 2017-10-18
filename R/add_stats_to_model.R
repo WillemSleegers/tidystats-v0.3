4c375d40cb53e8827a2198728f78241a701fb1cd
Willem Sleegers <w.sleegers@me.com>
2017-10-18 19:41:09 +0200

    Added function to add statistics to an existing model

---
+++ R/add_stats_to_model.R
@@ -0,0 +1,54 @@
+#' Add statistical output to a model in a tidy stats list
+#'
+#' \code{add_stats_to_model} adds output to a model in a tidy results list. Sometimes you have to run additional analyses on the output of a statistical test, so you want to add these results to an existing model in a tidy stats list.
+#'
+#' @param output output of a statistical test.
+#' @param results a tidy stats list.
+#' @param identifier a character string identifying the model.
+#' @param statistics a vector of statistics to select from the output and add to the model in the tidy stats list.
+#'
+#' @examples
+#' # Create an empty list to store the results in
+#' results <- list()
+#'
+#' # Conduct statistical tests
+#' model <- lm(extra ~ group, data = sleep)
+#'
+#' # Add output to the results list
+#' results <- add_stats(model, results, identifier = "M1")
+#'
+#' # Get confidence intervals of the model
+#' model_CIs <- confint(model)
+#'
+#' # Produce a tidy data frame of the CIs and add it to the results list
+#' results <- model_CIs %>%
+#'   tidy_stats_confint() %>%
+#'   add_stats_to_model(results, identifier = "M1")
+#'
+#' @export
+
+add_stats_to_model <- function(output, results, identifier, statistics = NULL) {
+
+  # Extract the results of the specific model through its identifier
+  res <- results[[identifier]]
+
+  # Create the new element
+  new_element <- output
+
+  # Filter out statistics
+  if (!is.null(statistics)) {
+    new_element <- filter(new_element, statistic %in% statistics)
+  }
+
+  # Merge with the model statistics
+  new_element <- full_join(res, new_element, by = c("term", "statistic", "value")) %>%
+    group_by(term) %>%
+    mutate(term_nr = first(term_nr)) %>%
+    arrange(term_nr) %>%
+    ungroup()
+
+  # Replace the model statistics
+  results[[identifier]] <- new_element
+
+  return(results)
+}
