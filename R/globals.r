d6acd9fce9c15865e60ffef312d96864252a56d8
Willem Sleegers <w.sleegers@me.com>
2017-10-24 20:20:03 +0200

    Added globalVariables to deal with R CMD check

---
+++ R/globals.r
@@ -0,0 +1,4 @@
+globalVariables(c(".", "identifier", "term_nr", "term", "statistic", "value", "median", "sd", "SD",
+                  "Df", "Sum Sq", "pf", "Mean Sq", "F value", "logLik", "Chisq", "Chi Df", "Res.Df",
+                  "Pr(>Chisq)", "Estimate", "Std. Error", "t value", "Pr(>|t|)", "Sum of Sq",
+                  "Pr(>F)", "pct_group"))
