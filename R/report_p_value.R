56723f34e322284c3b462b7bc1d9c910f621dc10
WillemSleegers <w.sleegers@me.com>
2017-12-17 21:01:49 +0100

    Improved report_p_value to deal with multiple p-values

--- R/report_p_value.R
+++ R/report_p_value.R
@@ -2,19 +2,22 @@
 #'
 #' A helper function to report p values in APA style.
 #'
-#' @param p_value A p-value.
+#' @param p_value One or more p-values.
 #'
 #' @examples
 #' report_p_value(0.532)
 #' report_p_value(0.0432)
 #' report_p_value(0.0000121)
+#' report_p_value(0.321, 0.0312, 0.00000098)
 #'
 #' @export
 
 report_p_value <- function(p_value) {
-  if (p_value >= .001) {
-    paste("*p* =", gsub(pattern = "0\\.", replacement = ".", x = format(p_value, digits = 2)))
-  } else {
-    paste("*p* < .001")
-  }
+
+  p_value <- if_else(p_value >= .001,
+                     paste("*p* =", gsub(pattern = "0\\.", replacement = ".",
+                                         x = round(p_value, digits = 3))),
+                     paste("*p* < .001"))
+
+  return(p_value)
 }
