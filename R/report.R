2bad00dc19dfca19462f9f4df8ed2405659055be
WillemSleegers <w.sleegers@me.com>
2018-01-20 01:07:06 +0100

    Added support for reporting descriptives.

--- R/report.R
+++ R/report.R
@@ -37,68 +37,91 @@ report <- function(identifier, term = NULL, term_nr = NULL, var = NULL, group = 
   # Check whether the identifier exists
   if (!identifier %in% names(results)) {
     stop("Identifier not found.")
+  } else {
+    res <- results[[identifier]]
   }
 
-  # Determine whether descriptives are requested or model results
+  if ("method" %in% names(res)) {
+    method <- res$method[1]
 
-
-  res_term <- term
-  res_term_nr <- term_nr
-  res_statistic <- statistic
-
-  # Find out which test was used
-  res <- results[[identifier]]
-  method <- res$method[1]
-
-  # Run the appropriate report function
-  if (grepl("t-test", method)) {
-    output <- report_t_test(results, identifier, statistic)
-  } else {
-    if (grepl("correlation", method)) {
-      output <- report_correlation(results, identifier, statistic)
+    # Run the appropriate report function
+    if (grepl("t-test", method)) {
+      output <- report_t_test(results, identifier, statistic)
     } else {
-      if (grepl("regression", method)) {
-        output <- report_lm(results, identifier, term, term_nr, statistic)
+      if (grepl("correlation", method)) {
+        output <- report_correlation(results, identifier, statistic)
       } else {
-        if (grepl("ANOVA|ANCOVA", method)) {
-          output <- report_anova(results, identifier, term, term_nr, statistic)
+        if (grepl("regression", method)) {
+          output <- report_lm(results, identifier, term, term_nr, statistic)
         } else {
-          output <- NULL
+          if (grepl("ANOVA|ANCOVA", method)) {
+            output <- report_anova(results, identifier, term, term_nr, statistic)
+          } else {
+            output <- NULL
+          }
         }
       }
     }
   }
 
-  # If output is null, it means we do not yet have a reporting function
-  # However, we can still report one statistic at the time, if one is provided
+  # If output is null, it means we either do not have the report function for that method, or the
+  # results are descriptives. In this case we can report a single statistic if enough information
+  # is provided.
   if (is.null(output)) {
-    if (!is.null(res_statistic)) {
+    res_statistic <- statistic
 
-      # Check if term information is required
-      if ("term" %in% names(res)) {
-        if (sum(!is.na(res$term)) > 0) {
-          if (is.null(term) & is.null(term_nr)) {
-            stop("No term information found.")
+    if (is.null(res_statistic)) {
+      stop("Cannot report multiple statistics, please provide a single statistic.")
+    } else {
+      if ("var" %in% names(res)) {
+        res_var <- var
+        if (length(unique(res$var)) > 1 & is.null(res_var)) {
+          stop("var information required.")
+        } else {
+          if ("group" %in% names(res)) {
+            res_group <- group
+            if (is.null(res_group)) {
+              stop("group information required.")
+            } else {
+              if (length(unique(res$var)) == 1 & "group" %in% names(res)) {
+                output <- format(filter(res, statistic == res_statistic & group == res_group)$value,
+                                 digits = 2, nsmall = 2)
+                } else {
+                  output <- format(filter(res, statistic == res_statistic & var == res_var &
+                                            group == res_group)$value, digits = 2, nsmall = 2)
+                  }
+                }
+          } else {
+            if (length(unique(res$var)) == 1) {
+              output <- format(filter(res, statistic == res_statistic)$value, digits = 2,
+                               nsmall = 2)
+            } else {
+              output <- format(filter(res, statistic == res_statistic & var == res_var)$value,
+                                 digits = 2, nsmall = 2)
+              }
+            }
+          }
+        } else {
+        if (length(unique(res$term)) > 1 & (is.null(term) & is.null(term_nr))) {
+          print("term information required.")
+        } else {
+          if (length(unique(res$term)) == 1) {
+            output <- format(filter(res, statistic == res_statistic)$value, digits = 2, nsmall = 2)
           } else {
             if (!is.null(term)) {
+              res_term <- term
               output <- format(filter(res, term == res_term & statistic == res_statistic)$value,
                                digits = 2, nsmall = 2)
             } else {
+              res_term_nr <- term_nr
               output <- format(
                 filter(res, term_nr == res_term_nr & statistic == res_statistic)$value,
                 digits = 2, nsmall = 2)
             }
           }
         }
-      } else {
-        output <- format(filter(res, statistic == res_statistic)$value, digits = 2, nsmall = 2)
       }
     }
-  }
-
-  # If it is still null, no statistic was supplied
-  if (is.null(output)) {
-    stop("Reporting for this method is not supported (but try reporting a single statistic at the time).")
   }
 
   return(output)
