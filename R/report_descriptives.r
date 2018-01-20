0179d93c4f3e4e2bf539b31d811a22772045faea
WillemSleegers <w.sleegers@me.com>
2018-01-20 01:07:26 +0100

    Added 2 convenient descriptives functions: M() and SD().

--- R/report_descriptives.r
+++ R/report_descriptives.r
@@ -27,46 +27,11 @@
 #' @import dplyr
 #'
 #' @export
-D <- function(list, identifier, var, group = NULL, statistic = NULL) {
-
-  # Check whether the identifier exists
-  if (!identifier %in% names(list)) {
-    stop("Identifier not found.")
-  }
-
-  # Create identification vars
-  var_res <- var
-  group_res <- group
-  statistic_res <- statistic
-
-  # Extract the data frame from the list
-  df <- magrittr::extract2(list, identifier)
-
-  # Extract the specific descriptive to be reported
-  if ("group" %in% names(df)) {
-    output <- filter(df,
-                     var == var_res,
-                     group == group_res,
-                     statistic_res == statistic)
-  } else {
-    output <- filter(df,
-                     var == var_res,
-                     statistic_res == statistic)
-  }
-
-  output <- pull(output, value)
-
-  return(output)
+M <- function(identifier, var = NULL, group = NULL, results = getOption("tidystats_list")) {
+  report(identifier = identifier, var = var, group = group, results = results, statistic = "M")
 }
 
-#' @rdname D
 #' @export
-M <- function(list, identifier, var, group = NULL) {
-  D(list = list, identifier = identifier, var = var, group = group, statistic = "M")
-}
-
-#' @rdname D
-#' @export
-SD <- function(list, identifier, var, group = NULL) {
-  D(list = list, identifier = identifier, var = var, group = group, statistic = "SD")
+SD <- function(identifier, var = NULL, group = NULL, results = getOption("tidystats_list")) {
+  report(identifier = identifier, var = var, group = group, results = results, statistic = "SD")
 }
