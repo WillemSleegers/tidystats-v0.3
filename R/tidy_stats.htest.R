6e527eda35a5ea50c6d5b7c72cb960f66b814f1d
WillemSleegers <w.sleegers@me.com>
2018-03-27 08:58:48 +0200

    Fixed issue that a confidence interval was always assumed to be 95% and added support for chi-square and wilcoxon tests.

--- R/tidy_stats.htest.R
+++ R/tidy_stats.htest.R
@@ -17,26 +17,33 @@ tidy_stats.htest <- function(model) {
 
   # Extract statistics
   output <- dplyr::bind_rows(
-    tibble::data_frame(statistic = names(model$estimate), value = model$estimate),
+    if (!is.null(model$estimate)) {
+      tibble::data_frame(statistic = names(model$estimate), value = model$estimate)
+    },
     tibble::data_frame(statistic = names(model$statistic), value = model$statistic),
     if (!is.null(model$parameter)) {
       tibble::data_frame(statistic = names(model$parameter), value = model$parameter)
     },
     tibble::data_frame(statistic = "p", value = model$p.value),
     if (!is.null(model$conf.int)) {
+      level <- attr(model$conf.int, "conf.level")*100
       tibble::data_frame(
-        statistic = c("95% CI lower", "95% CI upper"),
+        statistic = c(paste0(level, "% CI lower"), paste0(level, "% CI upper")),
         value = c(model$conf.int[1], model$conf.int[2])
       )
     },
-    tibble::data_frame(statistic = "null value", value = model$null.value)
+    if (!is.null(model$null.value)) {
+      tibble::data_frame(statistic = "null value", value = model$null.value)
+    }
   )
 
   # Add the method (use trimws to remove the leading space from a Two Sample t-test)
   output$method <- trimws(model$method)
 
   # Add additional information
-  output$notes <- paste(model$alternative, "test")
+  if (!is.null(model$alternative)) {
+    output$notes <- paste("alternative hypothesis:", model$alternative)
+  }
 
   return(output)
 }
