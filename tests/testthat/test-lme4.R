e12920b07493d24384dc48d40385f078d51f9c37
ikbentimkramer <53525709+ikbentimkramer@users.noreply.github.com>
2019-08-25 17:48:18 +0200

    Add package dependencies tests
    Adding the tests to R CMD check uncovered some unstated package
    dependencies. This commit adds those packages to DESCRIPTION (as
    suggested packages) and makes sure the dependent tests are not run if
    the required packages are not installed (which is possible, because
    they are suggested).

--- tests/testthat/test-lme4.R
+++ tests/testthat/test-lme4.R
@@ -28,8 +28,8 @@ test_that("lme4's linear mixed models with uncorrelated terms work", {
 })
 
 test_that("lme4's linear mixed models with dummies work", {
+  skip_if_not_installed("nlme")
   correct <- test_results[["lme4_lme_dummies"]]
-
   data(Orthodont, package = "nlme")
   Orthodont$nsex <- as.numeric(Orthodont$Sex == "Male")
   Orthodont$nsexage <- with(Orthodont, nsex * age)
